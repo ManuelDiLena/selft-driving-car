7072dcad9070a963e37acd3d65a293f2db16d866
ManuelDiLena <manuel.dilena.29@gmail.com>
2024-09-10 18:27:57 +0200

    Collision with traffic is detected

--- car.js
+++ car.js
@@ -37,6 +37,11 @@ class Car {
         return true;
       }
     }
+    for (let i = 0; i < traffic.length; i++) {
+      if (polysIntersect(this.polygon, traffic[i].polygon)) {
+        return true;
+      }
+    }
     return false;
   }
 
